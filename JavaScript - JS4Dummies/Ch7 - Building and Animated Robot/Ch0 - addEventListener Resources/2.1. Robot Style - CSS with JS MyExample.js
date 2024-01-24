8d3882f869423462ba5ccb3ab13e2462c19c508d
Karabo Tabane <61653111+brownEFX@users.noreply.github.com>
2024-01-24 17:53:12 +0200

    Add files via upload

---
+++ JavaScript - JS4Dummies/Ch7 - Building and Animated Robot/Ch0 - addEventListener Resources/2.1. Robot Style - CSS with JS MyExample.js
@@ -0,0 +1,8 @@
+document.getElementByTagName("p").style.fontFamily= "Google Sans,arial,sans-serif"; /*Helvetica,sans-serif; | Verdana,sans-serif;*/
+//document.getElementByTagName("p").style.fontFamily="Helvatica";
+
+document.getElementsByClassName("eye").style.backgroundColor = "purple";
+document.getElementsByClassName("arm").style.backgroundColor = "blue";
+
+document.getElementById("head").style.backgroundColor = "yellow";
+document.getElementById("righteye").style.backgroundColor = "purple";
