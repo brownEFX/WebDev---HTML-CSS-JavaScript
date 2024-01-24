771197fafed09c218a8029922f2820fb74b4627b
Karabo Tabane <61653111+brownEFX@users.noreply.github.com>
2024-01-24 17:44:55 +0200

    Add files via upload

---
+++ JavaScript - JS4Dummies/Ch2 - Understanding Syntax/Ch2.js
@@ -0,0 +1,10 @@
+//Printing a String
+let userName = "Kabo" 
+console.log("Hello, world. My name is " + userName);
+
+//escaping the quotation mark - using backslash before a quotation mark in a string.
+console.log("Hi, " + userName + ". Joe said, 'Hi.'");
+console.log("Hi, " + userName + '. Joe said, "Hi."');
+console.log("Hi, " + userName + ". Joe said, \"Hi.\"");
+//Printing the backslash:
+console.log("Hi, " + userName + ". Joe said, \\\"Hi.\"");
